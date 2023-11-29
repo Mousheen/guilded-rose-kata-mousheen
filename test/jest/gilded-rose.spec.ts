d6744c3b7bd7b560d79d605cb5257e2449b42212
Mousheen <mousheen_6@hotmail.com>
2023-11-29 19:43:35 +1300

    Write some initial tests
    write tests to see if its working correctly

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,18 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
+  it('should decrease the quality and sellIn values for naormal items', () => {
+    const gildedRose = new GildedRose([new Item('normal item', 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
   });
+
+  it('should not decrease quality below 0', () => {
+    const gildedRose = new GildedRose([new Item('normal item', 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
 });
